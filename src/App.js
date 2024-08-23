7bd1b171905d8ce3a1d13d54e569c38f2413a56e
睿 <122418084+Rui01sir@users.noreply.github.com>
2024-08-24 03:05:49 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
+import Company from './components/Company';
 import Navbar from './components/Navbar';
 import Page404 from './components/Page404';
-import Footer from './components/Footer'
-
+import Footer from './components/Footer';
+import TopButton from './components/TopButton';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar/>
+    <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        <Route path="Page404" element={<Page404/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
-    <Footer/>
-    </BrowserRouter>  
+      <TopButton />
+      <Footer />
+    </Router>
   );
 }
 
